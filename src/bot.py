b103e6100e6e4f961eba5e1abc9003b6374ae598
EnterNewName <lifez.memes@gmail.com>
2018-09-06 02:08:08 -0500

    added a nice startup event

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
-import discord, asyncio, json, pkg_resources
+import discord, asyncio, json, pkg_resources, time
 from discord.ext import commands
+from time import ctime
 
 with open("data.json") as data:
     config = json.load(data)
@@ -10,6 +11,16 @@ def get_prefix():
 bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, pm_help=False)
 cogs = config["bot"]["cogs"]
 
+@bot.event
+async def on_ready():
+    print("============================================")
+    print("Connected To Discord API, Loading Assets....")
+    print("============================================\nStats:\n")
+    print(f"Discord.py Version : " + pkg_resources.get_distribution("discord.py").version)
+    print(f"{bot.user} Is Online At {time}")
+    print(f"Guild Count : {len(bot.guilds)}\n")
+    print("Bot Version : 0.0.1\n")
+    
 if __name__ == '__main__':
     for module in cogs:
         bot.load_extension(module)
