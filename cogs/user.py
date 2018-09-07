062f971e49ae3c5e8945eeaade56ec2bc9d63cff
EnterNewName <lifez.memes@gmail.com>
2018-09-06 23:43:42 -0500

    oops lol forgot the `commands`

--- cogs/user.py
+++ cogs/user.py
@@ -10,14 +10,14 @@ class UserManagement:
         print("User Management Is Loaded")
     
     @commands.has_permissions(kick_members=True)
-    @bot.has_permissions(kick_members=True)
+    @commands.bot.has_permissions(kick_members=True)
     @commands.command(aliases=['k'])
     async def kick(self, ctx, user : discord.Member, reason=None):
         await user.kick(reason=reason)
         await ctx.send(f"**{user} has Been Kicked From The Guild With A Reason:** {reason}") 
 
     @commands.has_permissions(ban_members=True)
-    @bot.has_permissions(ban_members=True)
+    @commands.bot.has_permissions(ban_members=True)
     @commands.command()
     async def ban(self, ctx, user : discord.Member, reason=None):
         await user.ban(reason=reason)
