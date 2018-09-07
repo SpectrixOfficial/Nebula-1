6376e979cfab0d81d739a75df5801c46269a8429
EnterNewName <lifez.memes@gmail.com>
2018-09-06 23:41:25 -0500

    added ban and kick command

--- cogs/user.py
+++ cogs/user.py
@@ -8,7 +8,20 @@ class UserManagement:
      
     async def on_ready(self):
         print("User Management Is Loaded")
+    
+    @commands.has_permissions(kick_members=True)
+    @bot.has_permissions(kick_members=True)
+    @commands.command(aliases=['k'])
+    async def kick(self, ctx, user : discord.Member, reason=None):
+        await user.kick(reason=reason)
+        await ctx.send(f"**{user} has Been Kicked From The Guild With A Reason:** {reason}") 
 
+    @commands.has_permissions(ban_members=True)
+    @bot.has_permissions(ban_members=True)
+    @commands.command()
+    async def ban(self, ctx, user : discord.Member, reason=None):
+        await user.ban(reason=reason)
+        await ctx.send(f"**{user} has Been Banned From The Guild With A Reason:** {reason}") 
 
 def setup(bot):
     bot.add_cog(UserManagement(bot))
