bd87f101c617219e4a0cabe637d1b3aa8f217d04
EnterNewName <lifez.memes@gmail.com>
2018-09-11 00:09:02 -0500

    got liedetector done

--- cogs/fun.py
+++ cogs/fun.py
@@ -10,25 +10,32 @@ class FunCommands:
         await ctx.send(f"**I Choose.... `{random.choice(['Rocks', 'Paper', 'Scissors'])}`**")
 
     
-    @commands.command()
-    async def liedetector(self, ctx, input):
+    @commands.command(aliases=['ld'])
+    async def liedetector(self, ctx, *, input):
         lieresult = random.randint(0, 100)
         if lieresult <= 0 <= 10:
-            result = "My Lie Detector Is Detecting that He Is Telling The Truth"
+            result = f"Seeing No Lies, {ctx.author.name}"
         elif lieresult <= 11 <= 15:
-            result = f"Detecting The Truth, Not Smelling Any Lies... *yet*"
-        elif lieresult <= 20 <= 30:
-            result = ":octagonal_sign:  Hol Up, I Sniff A Lie"
-        elif lieresult <= 40 <= 50:
-            result = "*I smell nothing but liesss*"
-        elif lieresult <= 60 <= 70:
-            result = "***BEEP BEEP BEEP BEEP BEEP***"
-        elif lieresult <= 80 <= 100:
-            result = f"<:Kermit:488902339416293387> Nigga You Lying {lieresult}%"
-        else:
-            result = "You Broke the Lie Detector, :cry:"
+            result = "I'm Not Smelling Any Lies Yet"
+        elif lieresult <= 16 <= 20:
+            result = "*Sniff, Sniff*, No Lies, But i am Getting Suspicious"
+        elif lieresult <= 21 <= 25:
+            result = "I Smell A Lie, Kiddo"         
+        elif lieresult <= 26 <= 30:
+            result = "***Lie Detector Reports 4 Lies Has Been Told***"
+        elif lieresult <= 31 <= 40:
+            result = "**WHATS THIS, RUBBISH LIES**"
+        elif lieresult <= 41 <= 50:
+            result = "LIES LIES LIES"
+        elif lieresult <= 51 <= 60:
+            result = "***BEEP BEEP BEEP BEEP BEEP :no_entry: ***"
+        elif lieresult <= 61 <= 89:
+            result = "Damn, you lie so much, even the devil won't accept you"
+        elif lieresult <= 90 <= 100:
+            result = f"<:Kermit:488902339416293387> WE ALL KNOW YOU ARE LYING!"
         emb = discord.Embed(color=discord.Color(value=0x186391))
         emb.set_author(name="Lie Detector Test")
+        emb.add_field(name="Lie Checker", value=input, inline=False)
         emb.add_field(name=f"Result: ", value=result, inline=False)
         emb.add_field(name="Lie Percentage Result: ", value=f"{lieresult}% Percent", inline=False)
         await ctx.send(embed=emb)
