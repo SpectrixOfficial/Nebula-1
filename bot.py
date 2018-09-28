6ceaa1dc78772b93c6586e9dada15ffad60d4479
EnterNewName <lifez.memes@gmail.com>
2018-09-28 16:32:09 -0500

    added logging

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-import discord, asyncio, json, pkg_resources, time, datetime, os, sqlite3
+import discord, asyncio, json, pkg_resources, time, datetime
+import logging
 from discord.ext import commands
 from time import ctime
 from discord.ext.commands import clean_content
@@ -6,23 +7,17 @@ from discord.ext.commands import clean_content
 with open("database/data.json") as f:
     config = json.load(f)
 
-#The following unselected is still in testing, im too lazy to care right now
-"""def get_prefix(msg):
-    default_prefix = ["<@487164011683774464>", "."]
-    connection = sqlite3.connect('database/data.db')
-    db = connection.cursor()
-    try:
-        db.execute("SELECT * FROM guilddata SELECT ?=? SELECT prefix", msg.guild.id)
-        connection.close()
-    except:
-        return default_prefix
-    connection.close()"""
-        
-
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(config["prefix"]), case_insensitive=True, clean_content=True, max_messages=300)
 bot.remove_command('help')
 cogs = config["cogs"]
 lt = datetime.datetime.utcnow()
+
+# Logs Error That Might Clog The Stdout
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 @bot.event
 async def on_ready():
@@ -32,7 +27,7 @@ async def on_ready():
     print(f"Discord.py Version : " + pkg_resources.get_distribution("discord.py").version)
     print(f"{bot.user} Is Online At {datetime.datetime.utcnow()}")
     print(f"Guild Count : {len(bot.guilds)}\n")
-    
+        
 @bot.command()
 async def uptime(ctx):
     delta_uptime = datetime.datetime.utcnow() - lt
@@ -40,11 +35,11 @@ async def uptime(ctx):
     minutes, seconds = divmod(remainder, 60)
     days, hours = divmod(hours, 24)
     await ctx.send(f"I have been running for **{days}** day(s), **{hours}** hour(s), **{minutes}** minute(s), **{seconds}** second(s)!")
-    
+
 if __name__ == '__main__':
     for module in cogs:
         print("\nLoading Bot Extension")
         bot.load_extension(module)
-    print("\nBot Extensions Are Ready,\nLoading Bot And Connecting Extensions..")
-    
+    print("\nBot Extensions Are Ready, Bot Ready To Go!")
+        
 bot.run(config["bottoken"], bot=True, reconnect=True)
--- cogs/fun.py
+++ cogs/fun.py
@@ -1,4 +1,4 @@
-import discord, asyncio, json, random, math, os
+import discord, asyncio, json, random, math, os, aiohttp
 from discord.ext import commands
 from discord.ext.commands import clean_content
 
@@ -67,6 +67,6 @@ class FunCommands:
         embed.set_author(icon_url=user.avatar_url, name=str(user))
         embed.add_field(name="\uFEFF", value=permissions)
         await ctx.send(embed=embed, content=None)
-
+   
 def setup(bot):
     bot.add_cog(FunCommands(bot))
