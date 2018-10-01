6e2deae3bd5abdc80a0aa1ef92cd315d454c2188
SpectrixOfficial <owenhutch123@gmail.com>
2018-10-01 10:00:16 +0200

    Added Rock Paper Scissors command

--- bot.py
+++ bot.py
@@ -1,22 +1,25 @@
-import discord, asyncio, json, pkg_resources, time, datetime
-import logging
+import discord, asyncio, json, pkg_resources, time, datetime, logging
 from discord.ext import commands
 from time import ctime
 
 with open("database/data.json") as f:
     config = json.load(f)
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(config["prefix"]), case_insensitive=True, clean_content=True, max_messages=300)
+bot = commands.AutoShardedBot(command_prefix=commands.when_mentioned_or(config["prefix"]), 
+                             case_insensitive=True,
+                             clean_content=True,
+                             max_messages=300,
+                             owner_id=373256462211874836)
+
 bot.remove_command('help')
 cogs = config["cogs"]
 lt = datetime.datetime.utcnow()
 
 @bot.event
 async def on_ready():
-    print("========================")
-    print("Connected To Discord API")
+    print("========================\nConnected To Discord API")
     print("========================\nStats:\n")
-    print(f"Discord.py Version : " + pkg_resources.get_distribution("discord.py").version)
+    print("Discord.py Version : " + pkg_resources.get_distribution("discord.py").version)
     print(f"{bot.user} Is Online At {datetime.datetime.utcnow()}")
     print(f"Guild Count : {len(bot.guilds)}\n")
         
@@ -26,12 +29,13 @@ async def uptime(ctx):
     hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
     minutes, seconds = divmod(remainder, 60)
     days, hours = divmod(hours, 24)
-    await ctx.send(f"I have been running for **{days}** day(s), **{hours}** hour(s), **{minutes}** minute(s), **{seconds}** second(s)!")
+    await ctx.send(f"I have been running for **{days}** day(s), **{hours}** hour(s),\
+                    **{minutes}** minute(s), **{seconds}** second(s)!")
 
 if __name__ == '__main__':
     for module in cogs:
-        print("\nLoading Bot Extension")
         bot.load_extension(module)
+        print(f"\nLoaded extention {module}")
     print("\nBot Extensions Are Ready, Bot Ready To Go!")
-        
-bot.run(config["bottoken"], bot=True, reconnect=True)
+    
+    bot.run(config["bottoken"], bot=True, reconnect=True)
