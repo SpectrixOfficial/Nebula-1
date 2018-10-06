aeb2c93d2027024086e944fe9f31083e9dc04164
EnterNewName <lifez.memes@gmail.com>
2018-10-06 08:47:27 -0500

    remade commands to look neater

--- cogs/user.py
+++ cogs/user.py
@@ -12,8 +12,30 @@ class UserManagement:
     @commands.command()
     async def ban(self, ctx, user : discord.Member, banReason=None):
         if ctx.author.id == 373256462211874836 or ctx.author == ctx.guild.owner or ctx.author.top_role > user.top_role:
-            await ctx.send("hmm")
-            
+            await user.ban(reason=banReason)
+            if not banReason:
+                await ctx.send(f"**<:tickYes:490607182010777620> I have banned {user} from the server**")
+            else:
+                await ctx.send(f"**<:tickYes:490607182010777620> I have banned {user} from the server because: {banReason}**")
+
+    @commands.command()
+    async def kick(self, ctx, user : discord.Member, kickReason=None):
+        if ctx.author.id == 373256462211874836 or ctx.author == ctx.guild.owner or ctx.author.top_role > user.top_role:
+            await user.kick(reason=kickReason)
+            if not kickReason:
+                await ctx.send(f"**<:tickYes:490607182010777620> I have kicked {user} from the server**")
+            else:
+                await ctx.send(f"**<:tickYes:490607182010777620> I have kicked {user} from the server because: {kickReason}**")
+    
+    @commands.command()
+    async def softban(self, ctx, user : discord.Member, softbanReason=None):
+        if ctx.author.id == 373256462211874836 or ctx.author == ctx.guild.owner or ctx.author.top_role > user.top_role:
+            await user.ban(reason=softbanReason)
+            await user.unban()
+            if not softbanReason:
+                await ctx.send(f"**<:tickYes:490607182010777620> I have softbanned {user} from the server**")
+            else:
+                await ctx.send(f"**<:tickYes:490607182010777620> I have softbanned {user} from the server because: {softbanReason}**")        
 
         
 # no <:tickNo:483288678437879808> 
