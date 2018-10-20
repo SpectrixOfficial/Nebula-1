ec2ccfafa9b15f9bc4906f7e281e89c038d6e786
EnterNewName <lifez.memes@gmail.com>
2018-10-20 18:25:11 -0500

    completed feedback shit in server exclus

--- cogs/supportserver.py
+++ cogs/supportserver.py
@@ -1,4 +1,4 @@
-import discord, asyncio
+import discord, asyncio, datetime
 from discord.ext import commands
 
 class SupportExclusive:
@@ -11,9 +11,24 @@ class SupportExclusive:
     @commands.command()
     async def reportbug(self, ctx, *, bug: str=None):
         if not bug:
-            return await ctx.author.send("You Need To Specify What the Bug is...\n\nExample:\n`.reportbug the kick command isnt working`")
-        embed = discord.Embed(color=discord.Color.red(), title=f"New Bug Report From {ctx.author.name} | ({ctx.author.id})")
-        embed.add_field(name="Complaint", value=bug)
+            try:
+                await ctx.author.send("You Need To Specify What the Bug is...\n\nExample:\n`.reportbug the kick command isnt working`")
+            except:
+                pass
+        embed = discord.Embed(timestamp=datetime.datetime.utcnow(), color=discord.Color.red(), title=f"New Bug Report From {ctx.author.name} | ({ctx.author.id})")
+        embed.add_field(name="Bug Report Complaint:", value='\n' + bug)
+        await self.bot.get_channel(495417184877674499).send(embed=embed)
+
+    @commands.command()
+    async def suggest(self, ctx, * , suggestion : str=None):
+        if not suggestion:
+            try:
+                await ctx.author.send("You Need To Specify What To Suggest...\n\nExample:\n`.suggest add a fn cmd`")
+            except:
+                pass
+        embed = discord.Embed(timestamp=datetime.datetime.utcnow(), color=discord.Color.green(), title=f"New Suggestion From {ctx.author.name} | ({ctx.author.id})")
+        embed.add_field(name="Suggestion", value='\n' + suggestion)
+        await self.bot.get_channel(495417184877674499).send(embed=embed)
 
 
 def setup(bot):
